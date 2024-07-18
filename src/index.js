45a0a381a533241f094c1071f4cc5efe94935c49
r <r@gmail.com>
2024-07-18 14:30:30 +0500

    router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/e-commerce">
         <App />
       </Router>
     </Provider>
