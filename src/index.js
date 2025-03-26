6ba7a2e2d937daef6c53a999410f520ca3d2d2f3
r <r@gmail.com>
2025-03-26 08:26:59 +0500

    fix bags

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router basename="/e-commerce">
+      <Router basename="/e-commerce/">
         <App />
       </Router>
     </Provider>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Product } from "../slice/addCartSlice";
 
 export const store = configureStore({
+    devTools: true,
     reducer: {
         product: Product, 
-    },
-    devTools: true
+    }
 })
